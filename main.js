9338c605c5270856111aa212c9ae4168f754549d
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-02-19 16:55:47 +0300

    Update;- added firebaseSecurity middleware

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const serviceAccount = require('./service-account.json');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const rateLimit = require("express-rate-limit");
 const port = 5000;
 const add_customer_numbers = require('./controllers/add_customer_ids');
+//const {getFirebaseUser} = require('./helpers/firebaseSecurity');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
 const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
 app.use('/api', add_customer_numbers);
 app.get('/', (req, res) => res.send({message: "App works"}));
 app.listen(port, () => console.log('Running app 🤖🤖 ' + port));
