f576de59a9b50d02c86f1b6d233d415deffbf2a8
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-02-25 12:39:26 +0300

    Update;- xxxx

--- helpers/parallel_add.js
+++ helpers/parallel_add.js
@@ -48,7 +48,7 @@ async function WriteCustomerDetails(datas,count, res, name) {
 
 async function storeRandomisedWinners(count, luckyWinners, name){
     try{
-    const collection = admin.firestore().collection(`Yonja_week_${count}_winners`).doc(count);
+    const collection = admin.firestore().collection(`${name}_week_${count}_winners`).doc(count);
     collection.set(luckyWinners, {merge: true});
     clusterWeeklyLoosers(luckyWinners, count, name);
     }catch(e){
@@ -58,7 +58,7 @@ async function storeRandomisedWinners(count, luckyWinners, name){
 
 async function clusterWeeklyLoosers(luckyWinners, count, name){
     try{
-        const collection = admin.firestore().collection(`Yonja_week_${count}_customer_points`);
+        const collection = admin.firestore().collection(`${name}_week_${count}_customer_points`);
         await Promise.all(luckyWinners.map((winner) => {
             collection.where('customerId', '==', winner['customerId']).get().then((winner_id) => {
                 winner_id.forEach(x => {
