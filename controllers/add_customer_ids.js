643c924ca2bf30dc5db0bd685bd73ffdfc5f1ef9
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-03-11 00:29:36 +0300

    Update;-hard coded stuff

--- controllers/add_customer_ids.js
+++ controllers/add_customer_ids.js
@@ -5,9 +5,9 @@ const {ParallelIndividualWrites, RandomiseLuckyWinners, WriteCustomerDetails,
 
 
 router.post('/add-customer-details',  async (req, res) => {
-    const customerIds = req.body['payload'];
-    const name = req.body['name'];
-    const count = req.body['count'];
+    const customerIds = 'https://firebasestorage.googleapis.com/v0/b/wholesaleduuka-418f1.appspot.com/o/NCBA%2F1615411587705_Test%20file%208.csv?alt=media&token=41f001fa-5bff-4d5a-9fca-d4f12fde17dd';
+    const name = 'Holla';
+    const count = 8;
     console.log(customerIds);
     console.log(req.body)
     //url, count, res, name
@@ -18,12 +18,6 @@ router.post('/job', (req, res) =>{
     getJobId(req, res);
 });
 
-router.post('/add-customer-details', async (req, res, next) => {
-    const customerIds = req.body['payload'];
-    const name = req.body['name'];
-    const count = req.body['count'];
-    await WriteCustomerDetails(customerIds, count, res, name);
-});
 
 router.post('/randomise-lucky-winners', async (req, res, next) => {
     const name = req.body['name'];
