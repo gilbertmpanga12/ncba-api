caaa5bdaf6bb875d757849b7ed937da693e4b3f6
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-03-11 15:43:30 +0300

    Update;- fixed post bug

--- helpers/parallel_add.js
+++ helpers/parallel_add.js
@@ -9,7 +9,9 @@ const { firestore } = require('firebase-admin');
 let Queue = require('bull');
 // Connect to a local redis intance locally, and the Heroku-provided URL in production
 // redis://BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW@127.0.0.1:6379
-let REDIS_URL = process.env.HEROKU_REDIS_GOLD_URL || 'redis://BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW@127.0.0.1:6379';
+let REDIS_URL =
+  process.env.HEROKU_REDIS_GOLD_URL ||
+  "redis://127.0.0.1:6379";
 // {redis: {port: 6379, host: '127.0.0.1', password: 'BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW'}}
 let workQueue = new Queue('work', {redis: {port: 6379, host: '127.0.0.1', password: 'BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW'}});
 
