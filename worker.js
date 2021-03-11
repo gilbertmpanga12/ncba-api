b01a0be5f5cdbc257eb0f6798419866f754ba6ea
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-03-11 14:48:43 +0300

    Update;- updated 500_batch uploder

--- worker.js
+++ worker.js
@@ -1,105 +1,145 @@
-let throng = require('throng');
+let throng = require("throng");
 let Queue = require("bull");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 // const pickRandom = require('pick-random');
 const csv = require("csv-stream");
-const {logger} = require('./helpers/logger');
-const { nanoid } = require('nanoid');
-const { firestore } = require('firebase-admin');
-const request = require('request');
-const progress = require('request-progress');
+const { logger } = require("./helpers/logger");
+const { nanoid } = require("nanoid");
+const { firestore } = require("firebase-admin");
+const request = require("request");
+const progress = require("request-progress");
 const options = {
-  
-    columns : ['Customer Number', 'Loan Reference', 'Loan Start Date', 'Loan Repaid Date']
-}
+  columns: [
+    "Customer Number",
+    "Loan Reference",
+    "Loan Start Date",
+    "Loan Repaid Date",
+  ],
+};
 // Connect to a local redis instance locally, and the Heroku-provided URL in production
-let REDIS_URL = process.env.HEROKU_REDIS_GOLD_URL || "redis://BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW@127.0.0.1:6379";
+let REDIS_URL =
+  process.env.HEROKU_REDIS_GOLD_URL ||
+  "redis://BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW@127.0.0.1:6379";
 
 // Spin up multiple processes to handle jobs to take advantage of more CPU cores
 // See: https://devcenter.heroku.com/articles/node-concurrency for more info
 let workers = process.env.WEB_CONCURRENCY || 2;
 
 // The maximum number of jobs each worker should process at once. This will need
-// to be tuned for your application. If each job is mostly waiting on network 
+// to be tuned for your application. If each job is mostly waiting on network
 // responses it can be much higher. If each job is CPU-intensive, it might need
 // to be much lower.
 // to be refactored
 let maxJobsPerWorker = 50;
 
-const serviceAccount = require('./service-account.json');
+const serviceAccount = require("./service-account.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 function start() {
   // Connect to the named work queue
   // {redis: {port: 6379, host: '127.0.0.1', password: 'BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW'}}
-  
-  
-  
-  let workQueue = new Queue('work',{redis: {port: 6379, host: '127.0.0.1', password: 'BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW'}} );
+
+  let workQueue = new Queue("work", {
+    redis: {
+      port: 6379,
+      host: "127.0.0.1",
+      password:
+        "BpJqTatVLvgyUbTVT7Jv4BZLDyX6gaTERTuhlkTBXg3EV8MWjRk5uZI5EzRzR5OoW37lb+ONV8Ev9GOW",
+    },
+  });
 
   workQueue.process(maxJobsPerWorker, async (job) => {
-    try{
-        // const {url, name, count} = job.data;
-        const url = 'https://firebasestorage.googleapis.com/v0/b/wholesaleduuka-418f1.appspot.com/o/NCBA%2F1615411998226_Test%20file%208.csv?alt=media&token=bb9cac79-b72c-4fbf-9d1f-005491c79f53';
-        const name = 'Holla';
-        const count = 8;
-        let datas = [];
-        // const name = job.data['name'];
-        // const count = job.data['count'];
-        // const datas = job.data['user_details'];
-        const csvStream = csv.createStream(options);
-        progress(request(url)).on('progress', function (state) {
-            console.log('progress', state);
-        }).pipe(csvStream).on('error',function(err){
-                console.error(err);
-            })
-            .on('data', function(csv_data){
-               datas.push({
-            'Customer Number': csv_data['Customer Number'],
-            'Loan Reference': csv_data['Loan Reference'],
-            'Loan Repaid Date': csv_data['Loan Repaid Date'],
-            'Loan Start Date': csv_data['Loan Start Date']
+    try {
+      // const {url, name, count} = job.data;
+      const url =
+        "https://firebasestorage.googleapis.com/v0/b/wholesaleduuka-418f1.appspot.com/o/NCBA%2F1615411998226_Test%20file%208.csv?alt=media&token=bb9cac79-b72c-4fbf-9d1f-005491c79f53";
+      const name = "Holla";
+      const count = 8;
+      let datas = [];
+      // const name = job.data['name'];
+      // const count = job.data['count'];
+      // const datas = job.data['user_details'];
+      const csvStream = csv.createStream(options);
+      progress(request(url))
+        .on("progress", function (state) {
+          console.log("progress", state);
+        })
+        .pipe(csvStream)
+        .on("error", function (err) {
+          console.error(err);
+        })
+        .on("data", function (csv_data) {
+          datas.push({
+            "Customer Number": csv_data["Customer Number"],
+            "Loan Reference": csv_data["Loan Reference"],
+            "Loan Repaid Date": csv_data["Loan Repaid Date"],
+            "Loan Start Date": csv_data["Loan Start Date"],
+          });
+        })
+        .on("end", async function (data) {
+          // let points_500 = 500;
+          // let counter_break = 0;
+          // let all_count_length = datas.length;
+          // let customerPoints = firestore().batch();
+          // let customerDetails = firestore().batch();
+          // datas.forEach(customer_data => {
+          //     counter_break += 1;
+          //     if(counter_break === 500){
+          //         const payload = datas.splice(counter_break, all_count_length);
+          //         payload.forEach(user_data => {
+          //         const uid = nanoid(10);
+          //         console.log(user_data)
+                //   const points =  firestore().collection(`${name}_week_${count}_customer_points`).doc(uid); // points
+                //   customerPoints.set(points, {customerId: user_data['Customer Number'],
+                //   loanReference: user_data['Loan Reference'], uid});
+
+                //   const details = firestore().collection(`${name}_week_${count}_customer_details`).doc(uid); // details
+                //   customerDetails.set(details, {...user_data, uid});
+
+          //         });
+          //         customerPoints.commit();
+          //         customerDetails.commit();
+          //         customerPoints = firestore().batch();
+          //         customerDetails = firestore().batch();
+          //         counter_break = 0;
+          //         // final increments
+          //         // progress_points = 50;
+          //         //job.progress(progress_points);
+          //     }
+          // });
+
+          const documentSnapshotArray = datas;
+          const batchArrayPoints = [];
+          const batchArrayDetails = [];
+          batchArrayPoints.push(firestore.batch());
+          batchArrayDetails.push(firestore.batch());
+          let operationCounter = 0;
+          let batchIndex = 0;
+
+          documentSnapshotArray.forEach((csv_doc) => {
+            const documentData = csv_doc;
+            const uid = nanoid(10);
+            batchArrayPoints[batchIndex].set(uid, {customerId: documentData['Customer Number'],
+            loanReference: documentData['Loan Reference'], uid});
+            batchArrayDetails[batchIndex].set(uid, {...documentData, uid});
+            operationCounter++;
+
+            if (operationCounter === 499) {
+              batchArrayPoints.push(firestore.batch());
+              batchArrayDetails.push(firestore.batch());
+              batchIndex++;
+              operationCounter = 0;
+            }
+          });
+
+          batchArrayPoints.forEach(async (batch) => await batch.commit());
+          batchArrayDetails.forEach(async (batch) => await batch.commit());
         });
-               
-        }).on('end',function(data){
-            // let points_500 = 500;
-            let counter_break = 0;
-            let all_count_length = datas.length;
-            let customerPoints = firestore().batch();
-            let customerDetails = firestore().batch();
-            datas.forEach(customer_data => {
-                counter_break += 1;
-                if(counter_break === 500){
-                    const payload = datas.splice(counter_break, all_count_length);
-                    payload.forEach(user_data => {
-                    const uid = nanoid(10);
-                    console.log(user_data)
-                    const points =  firestore().collection(`${name}_week_${count}_customer_points`).doc(uid); // points
-                    customerPoints.set(points, {customerId: user_data['Customer Number'], 
-                    loanReference: user_data['Loan Reference'], uid});
-
-                    const details = firestore().collection(`${name}_week_${count}_customer_details`).doc(uid); // details
-                    customerDetails.set(details, {...user_data, uid});
-
-                    });
-                    customerPoints.commit();
-                    customerDetails.commit();
-                    customerPoints = firestore().batch();
-                    customerDetails = firestore().batch();
-                    counter_break = 0;
-                    // final increments
-                    // progress_points = 50;
-                    //job.progress(progress_points);
-                }
-            });
-        });
-       
-
-    }catch(e){
-        logger.info('WORKER ERROR', e);
+    } catch (e) {
+      logger.info("WORKER ERROR", e);
     }
   });
 }
