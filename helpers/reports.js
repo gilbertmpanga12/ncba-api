1efed112a747307dcb805e645f5504a576ced66a
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-03-17 11:37:48 +0300

    update; minor changes

--- helpers/reports.js
+++ helpers/reports.js
@@ -1,9 +1,8 @@
 const firebase = require('firebase-admin');
 const PdfPrinter = require('pdfmake');
-const { v4: uuidv4 } = require('uuid');
 const { Parser } = require('json2csv');
 const expirydate = {action: 'read', expires: '03-09-2500'};
-var fs = require('fs');
+const nanoid = require('nanoid');
 
 
 async function printPdf(fonts, docDefinition, res){
@@ -11,23 +10,20 @@ async function printPdf(fonts, docDefinition, res){
 		let printer = new PdfPrinter(fonts);
 		let pdfDoc = printer.createPdfKitDocument(docDefinition);
 		const bucket = firebase.storage().bucket('wholesaleduuka-418f1.appspot.com');
-		const gcsname = `${uuidv4()}.pdf`;
+		const gcsname = `${nanoid(10)}.pdf`;
 		const file = bucket.file(gcsname);
 		let stream = file.createWriteStream({
 			metadata: {
 				contentType: 'application/pdf'
 			}
 		});
-	    pdfDoc.pipe(stream).on('error', (err) => {
-			console.log(err);
-		}).on('finish', (e) => {
-			file.getSignedUrl(expirydate).then(url => {
-				const pdfUrl = url[0];
-				res.status(200).json({pdfUrl: pdfUrl});
-				
-				});
-		});
+	    pdfDoc.pipe(stream);
 		pdfDoc.end();
+		file.getSignedUrl(expirydate).then(url => {
+			const pdfUrl = url[0];
+			res.status(200).json({pdfUrl: pdfUrl});
+			
+			});
 	}catch(e){
 		console.log('PDF CREATION ERROR', e);
 		res.status(500).send({message: e});
@@ -39,7 +35,7 @@ async function printCsv(fullReuslts, res){
 	const json2csvParser = new Parser();
 	const csv = json2csvParser.parse(fullReuslts);
 	const bucket = firebase.storage().bucket('wholesaleduuka-418f1.appspot.com');
-	  const gcsname = `${uuidv4()}.csv`;
+	  const gcsname = `${nanoid(10)}.csv`;
 	const file = bucket.file(gcsname);
 	file.save(csv, function(err){
 	  if(err) throw err;
