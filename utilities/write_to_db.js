6fefdb8893625946efd816dc744d44b4d34cc86b
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-05-01 07:54:21 +0300

    Update;-added logs to debug mongo

--- utilities/write_to_db.js
+++ utilities/write_to_db.js
@@ -3,10 +3,13 @@ const stream = require('stream');
 function storeData(dbCollection){
     const csvOutputStream = new stream.Writable({objectMode
     :true});
+    console.log("I am running now ***");
     csvOutputStream._write = (chunk , encoding, callback) => {
+        console.log(chunk );
         dbCollection.insertMany(chunk).then(() => {
             callback();
         }).catch((err) => {
+            console.log('Error inserting',err);
             callback(err);
         });
     };
